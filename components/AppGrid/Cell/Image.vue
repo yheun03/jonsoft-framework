cbc6ca2f66858ae0e747034e49679b3356be82ad
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2026-03-17 16:30:43 +0900

    feat: 이미지 필드에 드래그 앤 드롭 기능 추가
    - AppImageField를 감싸는 div를 추가하여 드래그 앤 드롭 이벤트를 처리하도록 변경했습니다.
    - 사용자 경험을 향상시키기 위해 allow-drop 속성을 활성화했습니다.

--- components/AppGrid/Cell/Image.vue
+++ components/AppGrid/Cell/Image.vue
@@ -1,5 +1,13 @@
 <template>
-    <AppImageField v-model="value" />
+    <div
+        class="app-grid-cell-image"
+        @dragenter.stop
+        @dragover.stop
+        @dragleave.stop
+        @drop.stop
+    >
+        <AppImageField v-model="value" :allow-drop="true" />
+    </div>
 </template>
 
 <script setup lang="ts">
